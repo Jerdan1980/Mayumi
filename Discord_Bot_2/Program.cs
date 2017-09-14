771206f6ab30c15ace44904b16ee10fb845c79a2
Jerdan1980 <jeremytinana@gmail.com>
2017-09-14 11:09:56 -0400

    Bot actually does something

---
+++ Discord_Bot_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace Discord_Bot
+{
+    class Program
+    {
+        //declares discord client
+        static DiscordClient discord;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            //instatiates discord client
+            discord = new DiscordClient(new DiscordConfiguration {
+                Token = "",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e => {
+                if(e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
