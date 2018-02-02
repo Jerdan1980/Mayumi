dd1f3a4af57642efd17ef4ab7a2a1231f1571d30
Jerdan1980 <jeremytinana@gmail.com>
2018-02-01 21:20:53 -0500

    Update Driver.py

--- driver.py
+++ driver.py
@@ -1,6 +1,18 @@
-import discord
+#for token input
+import json
+#standard imports
+import random
+#for discord
 import asyncio
+import discord
 
+
+#import token file
+with open("tokens.json") as file:
+  tokens = json.load(file)
+file.closed
+
+#start of code discord implementation
 client = discord.Client()
 
 @client.event
@@ -12,5 +24,7 @@ async def on_message(message):
   if "mayumi" in message.content.toLower():
     if "ping" in message.content:
       await client.sendmessage(message.channel, 'pong!')
+    elif "time" in message.content:
+      await client.sendmessage(message.channel, message.timestamp)
 
-client.run(token) #insert way to find token here.
+client.run(tokens["discord"]) #insert way to find token here.
