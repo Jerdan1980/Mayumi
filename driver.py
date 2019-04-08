1911090672c4bc2e18f430160560f0ba53d01403
Jerdan1980 <jeremytinana@gmail.com>
2019-04-08 18:58:40 -0400

    make basic bot

--- driver.py
+++ driver.py
@@ -1,57 +1,43 @@
 #for token input
 import json
-#standard imports
-import random
 #for discord
 import asyncio
 import discord
+from discord.ext import commands
+#standard imports
+import random
 
+#load tokens
+filePath = input("token json filepath: ")
+with open(filePath) as file:
+    tokens = json.load(file)
+file.close()
 
-#import token file
-with open('tokens.json') as file:
-  tokens = json.load(file)
-file.closed
-
-#start of code discord implementation
-client = discord.Client()
-
-#upon startup of client
-@client.event
+#create discord bot
+bot = commands.Bot(command_prefix=tokens['prefix'])
+#start bot
+@bot.event
 async def on_ready():
-  print('reafy!')
+    print("Loaded!")
 
-#Jeremy's code
-@client.event
-async def on_message(message):
-  msg = message.content.lower()
-  if "mayumi" in msg:
-    #Jeremy's code
-    if "ping" in msg:
-      await client.send_message(message.channel, 'pong!')
-    elif "time" in msg:
-      await client.send_message(message.channel, message.timestamp)
-    elif ("flip" in msg) and ("coin" in msg):
-      coin = random.randint(0, 1)
-      if coin == 0:
-        await client.send_message(message.channel, 'heads')
-      elif coin == 1:
-        await client.send_message(message.channel, 'tails')
-      else:
-        await client.send_message(message.channel, 'something\'s wrong here')
-    elif ("should" in msg and "?" in msg) or ("8ball" in msg):
-      #trover helped me with the next line!
-      o = (lambda x:{None:x[0]}[random.shuffle(x)])(open("Resources\MayumiYesNo.txt", "r").read().split("\n"))
-      await client.send_message(message.channel, o)
-      
-    #Anissa's code
-    elif "writing prompts" in msg():
-      if"random" in msg:
-        await client.send_message(message.channel, file.open("Writing Prompts.txt").readlines()[random.randint(0, 148)].split("|")[0])
-      elif "humor" in msg:
-        await client.send_message(message.channel, file.open("Writing Prompts.txt").readlines()[random.randint(0, 148)].split("|")[0])
-      elif "fantasy" in msg:
-        await client.send_message(message.channel, file.open("Writing Prompts.txt").readlines()[random.randint(0, 148)].split("|")[0])
+#ping command
+@bot.command()
+async def ping():
+    await bot.say("pong!")
 
-    #Lukas' code
+#roll command
+#from sample bot
+@bot.command()
+async def roll(dice : str):
+    #Rolls a dice in NdN format.
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await bot.say('Format has to be in NdN!')
+        return
 
-client.run(tokens['discord']) #insert way to find token here.
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await bot.say(result)
+
+#bot token
+bot.run(tokens['discord'])
