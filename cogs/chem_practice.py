b1cb3e3c422fee8a06caee67166dd819042c722c
Jerdan1980 <jeremytinana@gmail.com>
2019-07-02 13:34:56 -0400

    add flametests

--- cogs/chem_practice.py
+++ cogs/chem_practice.py
@@ -33,13 +33,15 @@ class chem_practice(commands.Cog):
     #randomizer
     @commands.command()
     async def quiz(self, ctx):
-        pick = random.randrange(0, 3)
+        pick = random.randrange(0, 4)
         if pick == 0:
             await self.molsol(ctx)
         elif pick == 1:
             await self.solrules(ctx)
         elif pick == 2:
             await self.polyacids(ctx)
+        elif pick == 3:
+            await self.flametests(ctx)
 
     #molar solubility
     async def molsol(self, ctx):
@@ -49,7 +51,7 @@ class chem_practice(commands.Cog):
 
         #ask the question
         quest = f'Question for**{ctx.author.display_name}**:\n'
-        quest += f'\tYou have `{salt.name}` with Ksp {salt.Ksp}. What is the molar solubility?'
+        quest += f'\tYou have `{salt.name}` with Ksp {salt.Ksp}. What is the molar solubility? Assume the question does not require the use of the quadratic formula.'
         quest += '\nReply in format `submit <answer>`. Do not include units. Ex: `submit 3`. You have a 2% tolerance'
         await ctx.send(quest)
 
@@ -152,6 +154,43 @@ class chem_practice(commands.Cog):
                 await profile.add_pts(str(ctx.author.id), 1)
             else:
                 await ctx.send(f'Incorrect. The correct answer is {round(acid.pH, 3)}')
+
+    #flame tests
+    async def flametests(self,ctx):
+        #dict of chemicals
+        with open('resources/flametests.json', 'r') as f:
+            Ion_dict = json.load(f)
+        f.close()
+            
+        #shuffle the dict
+        temp = list(Ion_dict.keys())
+        random.shuffle(temp)
+        ion = temp[0]
+
+        #ask the question
+        quest = f'Question for **{ctx.author.name}**\n'
+        quest += f'\tWhat color is the flame test of {ion}?\n'
+        quest += 'Reply in format `submit <answer>`.Please respond either a color of the rainbow or white, brown, or black. Ex: `submit red`.\n'
+        quest += 'Any one of possible colors will be accepted as correct.'
+        await ctx.send(quest)
+        
+        #checker method
+        def check(msg):
+            return msg.content.startswith('submit') and msg.author == ctx.author and msg.channel == ctx.channel
+
+        #check the answer
+        try:
+            msg = await self.bot.wait_for('message', timeout=30.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send(f'Out of time! the correct answer(s) are ||{", ".join(Ion_dict[ion])}||')
+        else:
+            answer = msg.content.split()[1]
+            if answer.lower() in Ion_dict[ion]:
+                await ctx.send('Correct.')
+                profile = self.bot.get_cog('profile')
+                await profile.add_pts(str(ctx.author.id), 1)
+            else:
+                await ctx.send(f'Incorrect. The correct answer(s) are {", ".join(Ion_dict[ion])}.')
 
 
 def setup(bot):
