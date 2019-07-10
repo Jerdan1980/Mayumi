924c16c9008a520ccd9165343502cab0dfdc082b
Jerdan1980 <jeremytinana@gmail.com>
2019-07-10 13:36:51 -0400

    remove Kb values

--- cogs/chem_topics/acids.py
+++ cogs/chem_topics/acids.py
@@ -36,17 +36,12 @@ class acids(commands.Cog):
         acid = self.acid_list[0]
 
         #randomize the question
-        pick_K = bool(random.getrandbits(1))
         pick_pH = random.choice(["pH", "pOH"])
 
         #print out the question
         quest = f'Question for **{ctx.author.display_name}**. (1 point)\n'
         quest += f'\tA solution of `{acid.name}` has a '
-        if pick_K:
-            quest += "Ka1 of {:.3e} and the Ka2 of {:.3e}. ".format(acid.Ka1, acid.Ka2)
-        elif not pick_K:
-            quest += "Kb1 of {:.3e} and the Kb2 of {:.3e}. ".format(acid.Kb1, acid.Kb2)
-        quest += f'What is the {pick_pH}? Assume the question does not require the use of the quadratic formula.'
+        quest += "Ka1 of {:.3e} and the Ka2 of {:.3e}. ".format(acid.Ka1, acid.Ka2)
         quest += '\nReply in format `submit <answer>`. Do not include units. Ex: `submit 3`. You have a 2% tolerance'
         await ctx.send(quest)
 
@@ -85,7 +80,5 @@ class pH():
         self.name = acid_name
         self.Ka1 = float(K1)
         self.Ka2 = float(K2)
-        self.Kb1 = 1e-14 / self.Ka1
-        self.Kb2 = 1e-14 / self.Ka2
         self.pH = -1 * math.log10(pow(self.Ka1 * self.Ka2, 0.5 ))
         self.pOH = 14 - self.pH
