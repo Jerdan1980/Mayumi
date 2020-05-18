071e50c13ff51c2651fb711b22bedf53ce9d906a
Jerdan1980 <jeremytinana@gmail.com>
2020-05-18 18:43:44 -0400

    add sentient outposts

--- commands/warframe.js
+++ commands/warframe.js
@@ -20,13 +20,33 @@ module.exports = {
 							data.push(`**Cetus:** ${body.cetusCycle.state} for ${body.cetusCycle.timeLeft}`);
 							data.push(`**Orb Vallis:** ${body.vallisCycle.state} for ${body.vallisCycle.timeLeft}`);
 
-							message.channel.send(data, {split: true});
+							return message.channel.send(data, {split: true});
 						})
 						.catch((error) => {
-							message.channel.send(`Error: \`${error}\``);
+							return message.channel.send(`Error: \`${error}\``);
 						});	
 				} catch (error) {
-					message.channel.send(`Error: \`${error}\``)
+					return message.channel.send(`Error: \`${error}\``)
+				}
+				break;
+			case 'railjack':
+			case 'rj':
+				try {
+					axios.get('https://api.warframestat.us/pc')
+						.then((response) => {
+							const body = response.data;
+							
+							if(body.sentientOutposts.active) {
+								return message.channel.send(`Sentient outpost at \`${body.sentientOutposts.mission.node}\``);
+							} else {
+								return message.channel.send(`No sentient outposts active`);
+							}
+						})
+						.catch((error) => {
+							return message.channel.send(`Error: \`${error}\``);
+						});	
+				} catch (error) {
+					return message.channel.send(`Error: \`${error}\``)
 				}
 				break;
 			default:
