167908b5dd495419a60d2579380a2fba951d9588
Jerdan1980 <jeremytinana@gmail.com>
2020-05-22 17:34:43 -0400

    add nightwave command

--- commands/warframe.js
+++ commands/warframe.js
@@ -103,7 +103,36 @@ module.exports = {
 					return message.channel.send(formatError(error))
 				}
 				break;
-			break;
+			case 'nightwave':
+				try {
+					axios.get('https://api.warframestat.us/pc')
+						.then((response) => {
+							const body = response.data.nightwave;
+
+							if(body.active) {
+								const embed = new Discord.MessageEmbed()
+									.setTitle("Today's nightwave")
+									.addFields (
+										body.activeChallenges.map((challenge) => {
+											return {
+												name: `**${challenge.title}**: ${challenge.isDaily ? 'Daily' : 'Weekly'}, ${challenge.reputation}`,
+												value: challenge.desc
+											}
+										})
+									);
+
+								return message.channel.send(embed);
+							} else {
+								return message.channel.send("Nightwave is not active currently.");
+							}
+						})
+						.catch((error) => {
+							return message.channel.send(formatError(error));
+						});	
+				} catch (error) {
+					return message.channel.send(formatError(error))
+				}
+				break;
 			default:
 				message.channel.send('Incorrect arguments. please try again.');
 		}
