8395615e25642360982a20dd669449615260d691
EADChemistry <52047277+EADChemistry@users.noreply.github.com>
2020-05-22 21:25:59 -0400

    Create help.js
    Lol I tried

---
+++ commands/warframe/help.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'help wf',
+	description: 'help wf',
+	execute(message, args) {
+		message.channel.send('Commands include baro, nightwave, ralijack, sortie and time.');
+	}
+}
